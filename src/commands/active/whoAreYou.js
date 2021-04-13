dee86fbdda94d6ac40a6d4497742b11a0dd1394f
Xentradi <ikaika@xentradi.com>
2021-04-12 14:51:39 -1000

    removed ping data from whoareyou command

--- src/commands/active/whoAreYou.js
+++ src/commands/active/whoAreYou.js
@@ -7,11 +7,5 @@ module.exports = {
   execute(client, message, args) {
     //client.functions.commandReact(message, 1);
     message.channel.send(`Who is ${client.user.username}?! ${client.user.username} is your favorite mask inhabiting magic item dealer! ${client.user.username} also help you roll dice and keep track of items... Ok not that last one;  DM guy is pretty lazy and didn't prepare that just, like he didn't prepare for the next game.`)
-
-    message.channel.send('Ping?').then((m) => {
-      const latency = m.createdTimestamp - message.createdTimestamp;
-      const apiPing = Math.round(client.ws.ping);
-      m.edit(`Pong! Latency is ${latency}ms. API Latency is ${apiPing}ms.`);
-    });
   },
 };
