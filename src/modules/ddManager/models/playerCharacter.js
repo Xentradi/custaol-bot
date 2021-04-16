7679cb37052a53477eacfd85012ba4e10f003eb9
Xentradi <ikaika@xentradi.com>
2021-04-15 15:58:37 -1000

    started on playerCharacter model

--- src/modules/ddManager/models/playerCharacter.js
+++ src/modules/ddManager/models/playerCharacter.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
-const playerCharacterSchema = new Schema({
+
+const playerCharacterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: Number,
+        default: 1
+    },
+    race: {
+        type: String,
+        required: true
+    },
+    class: [String],
+    stats: {
+        type: Map,
+        of: Number
+    }
 
 })
 
