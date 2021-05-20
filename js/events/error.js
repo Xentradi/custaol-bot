542e5f6cfeac54242f53d7cebcb941d9737e2428
Xentradi <ikaika@xentradi.com>
2021-05-20 10:13:37 -1000

    re-write complete

--- js/events/error.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'error',
-    description: 'Prints errors to the console.',
-    execute: async (client, error) => {
-        console.error(error);
-    },
-};
--- js/events/guildCreate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'guildCreate',
-    description: 'What to do when joining a new server',
-    execute: async (client, guild) => {
-        console.log(`Joined new server: ${guild}`);
-    },
-};
